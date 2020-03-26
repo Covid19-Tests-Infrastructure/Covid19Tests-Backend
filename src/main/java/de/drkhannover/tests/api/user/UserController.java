65ceb08fcfaec7ff3622921373b4cdef36f63edf
Maze7 <marcel-maze7@hotmail.de>
2020-03-26 19:05:29 +0100

    User edit

--- src/main/java/de/drkhannover/tests/api/user/UserController.java
+++ src/main/java/de/drkhannover/tests/api/user/UserController.java
@@ -2,6 +2,9 @@ package de.drkhannover.tests.api.user;
 
 import static de.drkhannover.tests.api.util.NullHelpers.notNull;
 
+import java.util.Collection;
+import java.util.Optional;
+
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.validation.Valid;
@@ -12,6 +15,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -26,6 +30,7 @@ import de.drkhannover.tests.api.user.dto.UserDto;
 import de.drkhannover.tests.api.user.exceptions.MissingDataException;
 import de.drkhannover.tests.api.user.exceptions.UserEditException;
 import de.drkhannover.tests.api.user.jpa.User;
+import springfox.documentation.annotations.ApiIgnore;
 
 /**
  * @author Marcel
@@ -33,80 +38,86 @@ import de.drkhannover.tests.api.user.jpa.User;
 @RestController
 public class UserController {
 
-    private Logger log = LoggerFactory.getLogger(UserController.class);
+	private Logger log = LoggerFactory.getLogger(UserController.class);
 
-    @Autowired
-    private IUserService userService;
-    
-    @PutMapping(ControllerPath.USER_ADD)
-    public String addLocalUser(@RequestBody @NotNull @Valid NewUserDto newUserDto) throws UserEditException {
-        @Nonnull String username = notNull(newUserDto.username); // spring validation
-        @Nonnull String password = notNull(newUserDto.password); // spring validation
-        
-        final var newUser = User.createDefaultUser(username, password);
-        if (!userService.isUserInDatabase(newUser)) {
-            userService.storeUser(newUser);
-            //return newUser.toString();
-        } else {
-            log.info("No user added: Duplicate entry");
-            throw new UserEditException("Can't add user: Already existing");
-        }
-    }
+	@Autowired
+	private IUserService userService;
 
-    @PutMapping(ControllerPath.MANAGEMENT_EDIT_USER)
-    public void editLocalUser(@RequestBody @NotNull @Nonnull @Valid UserDto userDto, @Nullable Authentication auth)
-                              throws MissingDataException, UsernameNotFoundException, AccessViolationException {
-        if (auth == null) {
-            throw new InternalError("Authentication not received");
-        }
-        @Nullable User authenticatedUser = auth.getPrincipal();
-        boolean selfEdit = authenticatedUser.getUserName().equals(userDto.username);
-        
-        try {
-            if (authenticatedUser == null) {
-                log.warn("Unknown user type is logged in: " + auth.getPrincipal().toString());
-                throw new AccessViolationException("User not known.");
-            }
-            boolean selfEdit = authenticatedUser.getUserName().equals(userDto.username) 
-                    && authenticatedUser.getRealm().equals(userDto.realm);
-            if (authenticatedUser.getRole().equals(UserRole.ADMIN.name())) {
-                UserDto.adminUserDtoEdit(authenticatedUser, userDto);
-            } else if (selfEdit) {
-                UserDto.defaultUserDtoEdit(authenticatedUser, userDto);
-                userService.storeUser(authenticatedUser);
-            } else {
-                log.warn("User " + authenticatedUser.getUserName() + " tries to modify data of " + userDto.username);
-                throw new AccessViolationException("Could not edit other users data");
-            }
-        } catch (UserNotFoundException e) {
-            log.info("User is logged  in but no data is in the database. Maybe database is down?");
-            throw new UserNotFoundException("Could not edit user, reason: " + e.getMessage() + ". Maybe our databse"
-                    + " is offline or the logged in user was deleted.");
-        }
-    }
+	@PutMapping(ControllerPath.USER_ADD)
+	public String addLocalUser(@RequestBody @NotNull @Valid NewUserDto newUserDto) throws UserEditException {
+		@Nonnull
+		String username = notNull(newUserDto.username); // spring validation
+		@Nonnull
+		String password = notNull(newUserDto.password); // spring validation
+
+		final var newUser = User.createDefaultUser(username, password);
+		if (!userService.isUserInDatabase(newUser)) {
+			userService.storeUser(newUser);
+			return newUser.toString(); // TODO
+		} else {
+			log.info("No user added: Duplicate entry");
+			throw new UserEditException("Can't add user: Already existing");
+		}
+	}
+
+	@PutMapping(ControllerPath.USER_EDIT)
+	public void editLocalUser(@RequestBody @NotNull @Nonnull @Valid UserDto userDto, @ApiIgnore @Nullable Authentication auth)
+			throws MissingDataException, UsernameNotFoundException, AccessViolationException {
+		if (auth == null) {
+			throw new InternalError("Authentication not received");
+		}
+		String username = Optional.ofNullable(auth.getPrincipal()).orElse("").toString();
+		boolean selfEdit = username.equals(userDto.username);
+		UserRole role = authorityToRole(auth.getAuthorities());
+
+		try {
+			if (username.isBlank()) {
+				log.warn("Unknown user type is logged in: " + auth.getPrincipal().toString());
+				throw new AccessViolationException("User not known.");
+			}
+			User authenticatedUser = userService.findUserByUsername(username);
+			if (role == UserRole.ADMIN) {
+				UserDto.adminUserDtoEdit(authenticatedUser, userDto);
+			} else if (selfEdit) {
+				UserDto.defaultUserDtoEdit(authenticatedUser, userDto);
+				userService.storeUser(authenticatedUser);
+			} else {
+				log.warn("User " + username + " tries to modify data of " + userDto.username);
+				throw new AccessViolationException("Could not edit other users data");
+			}
+		} catch (UsernameNotFoundException e) {
+			log.info("User is logged  in but no data is in the database. Maybe database is down?");
+			throw new UsernameNotFoundException("Could not edit user, reason: " + e.getMessage() + ". Maybe our databse"
+					+ " is offline or the logged in user was deleted.");
+		}
+	}
+
+	public UserRole authorityToRole(Collection<? extends GrantedAuthority> collection) {
+		var singleAuthy = (GrantedAuthority) collection.toArray()[0];
+		return Enum.valueOf(UserRole.class, singleAuthy.getAuthority());
+	}
 
 //    @GetMapping("/user/delete")
-//    @GetMapping("/user/changepass")a
 
-    @ResponseStatus(code = HttpStatus.FORBIDDEN)
-    @ExceptionHandler(AccessViolationException.class)
-    public String handleUserEditException(AccessViolationException ex) {
-        return handleException(ex);
-    }
-    
-    @ResponseStatus(code = HttpStatus.NOT_MODIFIED)
-    @ExceptionHandler(MissingDataException.class)
-    public String handleUserNotFound(MissingDataException ex) {
-        return handleException(ex);
-    }
+	@ResponseStatus(code = HttpStatus.FORBIDDEN)
+	@ExceptionHandler(AccessViolationException.class)
+	public String handleUserEditException(AccessViolationException ex) {
+		return handleException(ex);
+	}
 
-    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
-    @ExceptionHandler({ UserNotFoundException.class, UserEditException.class })
-    public String handleException(Exception ex) {
-        if (ex.getMessage() == null || ex.getMessage().isEmpty()) {
-            return "There was a problem with the user data.";
-        } else {
-            return ex.getMessage();
-        }
-    }
+	@ResponseStatus(code = HttpStatus.NOT_MODIFIED)
+	@ExceptionHandler(MissingDataException.class)
+	public String handleUserNotFound(MissingDataException ex) {
+		return handleException(ex);
+	}
+
+	@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+	@ExceptionHandler({ UsernameNotFoundException.class, UserEditException.class })
+	public String handleException(Exception ex) {
+		if (ex.getMessage() == null || ex.getMessage().isEmpty()) {
+			return "There was a problem with the user data.";
+		} else {
+			return ex.getMessage();
+		}
+	}
 }

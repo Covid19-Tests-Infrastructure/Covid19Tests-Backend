5ccf90aa19d26ee2e07649a68e074f0012109d73
Maze7 <marcel-maze7@hotmail.de>
2020-03-27 16:14:09 +0100

    Validation in formular

--- src/main/java/de/drkhannover/tests/api/form/dto/FormDto.java
+++ src/main/java/de/drkhannover/tests/api/form/dto/FormDto.java
@@ -1,5 +1,6 @@
 package de.drkhannover.tests.api.form.dto;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 import de.drkhannover.tests.api.form.RKICriteria;
@@ -16,26 +17,39 @@ public class FormDto {
 	public Covid19Info info;
 	
 	public static class OrdererDto {
+		@NotBlank
 		public String firstname;
+		@NotBlank
 		public String lastname;
 		public String lanr;
 		public String bsnr;
+		@NotBlank
 		public AddressDto address;
+		@NotBlank
 		public String phoneNumber;
 		public String fax;
+		@NotBlank
 		public String email;
 	}
 
 	public static class PatientDto {
+		@NotBlank
 		public String occupationGroup;
+		@NotBlank
 		public String firstname;
+		@NotBlank
 		public String lastname;
+		@NotBlank
 		public String bday;
 		public AddressDto address;
+		@NotBlank
 		public String phoneNumber;
 		public String healthCareOrganisationNumber;
+		@NotBlank
 		public String personalHealthCareNumber;
+		@NotBlank
 		public String insuranceType;
+		@NotBlank
 		public boolean mobile;
 	}
 	

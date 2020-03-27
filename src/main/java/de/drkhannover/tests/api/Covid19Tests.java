e1f72b3da7477781a386ce9588ede5d95526738d
Maze7 <marcel-maze7@hotmail.de>
2020-03-27 02:32:15 +0100

    Move main class

--- src/main/java/de/drkhannover/tests/api/Covid19Tests.java
+++ src/main/java/de/drkhannover/tests/api/Covid19Tests.java
@@ -1,4 +1,4 @@
-package de.drkhannover.tests;
+package de.drkhannover.tests.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
